a5e4e18c94013ab13f452ab1d1e7a2d5b3301ce0
aldisypu <aldisypu@gmail.com>
2023-12-21 20:04:24 +0700

    belajar docker dasar

---
+++ docker-bind-mounts.sh
@@ -0,0 +1,1 @@
+docker container create --name mongodata --publish 27018:27017 --mount "type=bind,source=/Users/aldisypu/Dev/belajar-docker-dasar/mongo-data,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=admin --env MONGO_INITDB_ROOT_PASSWORD=root mongo:latest
